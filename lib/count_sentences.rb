0c29de2f4ef266dd35eb9004da02757820b2a834
prodigysurge <boatinglife6@yahoo.com>
2021-01-20 21:13:30 -0500

    commit tree 9221c0f21da85a3bccbb7ce8f572d89b9bc131db

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    sentence_string = self.split(/[.?!]/)
+    sentence_string.delete_if do |sentence|
+      sentence.empty?
+    end
+    sentence_string.count
   end
 end
